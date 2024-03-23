89a90bb4756548a32cc0bf35970a4565ea507f36
Samuel Brereton <sam@sambrereton.net.au>
2024-03-23 17:48:18 +1100

    .

--- config/app.php
+++ config/app.php
@@ -206,7 +206,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\GuzzleServiceProvider::class,
 
     ],
 
