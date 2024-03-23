2040abb15cf611c5dc61a3ae0e146af1990716da
Samuel Brereton <sam@sambrereton.net.au>
2024-03-23 17:34:35 +1100

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->extend(Http::class, function ($service, $app) {
+            return $service->withOptions([
+                'proxy' => 'http://10.129.16.19:8080',
+            ]);
+        });
     }
 
     /**
